bccb09999ae278f89f2d43fb18b4c0511eb95863
KaueJhefley <kauejhefley@gmail.com>
2026-08-19 14:18:42 -0300

    exercicio 2

--- teste/arquivo2.py
+++
@@ -1,5 +0,0 @@
-#questao que eu to com preguiça de escrever
-distancia_sousa_cajazeira=45
-tempo=0.5
-velocidade_media=distancia_sousa_cajazeira / tempo
-print('voce obteve',velocidade_media, 'km/h')
--- teste/ex14.py
+++
@@ -1,9 +0,0 @@
-nome = input('digite seu nome: ')
-idade = input('digite sua idade: ')
-salario = input('seu salario: ')
-sexo = input('sexo: ')
-ec = input('seu estado civil: ')
-
-while nome < 3:
-    nome = input('digite seu nome: ')
-
