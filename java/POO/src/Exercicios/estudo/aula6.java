b0430a44f6f5c157fa444eff9699bd96d16f089d
KaueJhefley <kauejhefley@gmail.com>
2026-08-20 17:04:44 -0300

    sla

---
+++ java/POO/src/Exercicios/estudo/aula6.java
@@ -0,0 +1,20 @@
+package Exercicios.estudo;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class aula6 {
+    public static void main(String[] args) {
+        String filme = JOptionPane.showInputDialog(null, "voce e gueba? ", "TESTE DE GUEBISSE", JOptionPane.ERROR_MESSAGE);
+        if (filme.equals("nao")) {
+        JOptionPane.showMessageDialog(null, "mentira", "o caba mentiroso", JOptionPane.INFORMATION_MESSAGE);
+        }
+        else if (filme.equals("sim")) {
+        JOptionPane.showMessageDialog(null, "Tudo bem cara", "ok", JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+
+    }
+
+}
